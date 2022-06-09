f3bf55b132f496060d40388cbb05d2051185418a
hyunha95 <nohyunha95@gmail.com>
2022-06-09 10:35:30 +0900

    &:hover

--- src/App.js
+++ src/App.js
@@ -1,48 +1,29 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: tomato;
+  &:hover {
+    color: teal;
+  }
+`;
+
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-  50% {
-    transform:rotate(360deg);
-    border-radius:100px;
-  }
-  100% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${rotationAnimation} 1s linear infinite;
-  ${Emoji}:hover {
-    font-size: 98px;
+  ${Title}:hover {
+    font-size: 99px;
+  }
   }
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">😉</Emoji>
-      </Box>
-      <Emoji>💥</Emoji>
+      <Title>Hello</Title>
+      <h1>Hello</h1>
     </Wrapper>
   );
 }
