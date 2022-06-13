dc9e72e20ccb01bf908d798838e3c3ff1f0dbcc4
hyunha95 <nohyunha95@gmail.com>
2022-06-13 18:02:37 +0900

    커밋

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Coin from "./routes/Coin";
 import Coins from "./routes/Coins";
 
 function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/:coinId" element={<Coin />}></Route>
-        <Route path="/" element={<Coins />}></Route>
-      </Routes>
+      <Switch>
+        <Route path="/:coinId">
+          <Coin />
+        </Route>
+        <Route path="/">
+          <Coins />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
